3d53963b3bb0b55d7dcf1cb9ecd04b551efc4fce
novakoki <shaoziqi0628@gmail.com>
2015-07-11 16:19:37 +0800

    modified

--- js/helper.js
+++ js/helper.js
@@ -23,7 +23,7 @@ var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img src="%data%" class="biopic" alt="A picture of mine">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
@@ -40,16 +40,16 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" alt="Project Image">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em><br></br>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h3 style="padding:0px">Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
@@ -110,7 +110,7 @@ function initializeMap() {
   };
 
 
-  map = new google.maps.Map(document.querySelector('#map-div'), mapOptions);
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
@@ -170,6 +170,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -232,11 +233,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
+// window.addEventListener('resize', function(e) {
+//   //Make sure the map bounds get updated on page resize
 //  map.fitBounds(mapBounds);
-//});
+// });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,161 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var work = {
+	"jobs":[
+	{
+		"employer":"",
+		"title":"",
+		"location":"",
+		"dates":"",
+		"description":""
+	}
+	],
+	"display":function () {
+		for(job in work.jobs){
+			$('#workExperience').append(HTMLworkStart);
+			$('.work-entry:last').append(HTMLworkEmployer.replace('%data%',work.jobs[job].employer)
+				+HTMLworkTitle.replace('%data%',work.jobs[job].title));
+			$('.work-entry:last').append(HTMLworkLocation.replace('%data%',work.jobs[job].location));
+			$('.work-entry:last').append(HTMLworkDates.replace('%data%',work.jobs[job].dates));
+			$('.work-entry:last').append(HTMLworkDescription.replace('%data%',work.jobs[job].description));	
+		}
+	}	
+};
+
+var projects = {
+	"projects":[
+	{
+		"title":"Voice-Trip",
+		"dates":"2014",
+		"description":"An Android app designed for travellers",
+		"images":"http://placekitten.com/550/250",
+		"url":"https://github.com/novakoki/Voice-Trip"
+	},
+	{
+		"title":"Rhythm-Snake",
+		"dates":"2013",
+		"description":"A modified classic snake game",
+		"images":"http://placekitten.com/g/550/250",
+		"url":"https://github.com/novakoki/Rhythm-Snake"
+	}
+	],
+	"display":function () {
+		for(project in projects.projects){
+			$('#projects').append(HTMLprojectStart);
+			$('.project-entry:last').append(HTMLprojectTitle.replace('%data%',projects.projects[project].title));
+			$('.project-entry:last').append(HTMLprojectDates.replace('%data%',projects.projects[project].dates));
+			$('.project-entry:last').append(HTMLprojectDescription.replace('%data%',projects.projects[project].description));
+			$('.project-entry:last').append(HTMLprojectImage.replace('%data%',projects.projects[project].images));
+			$('.project-entry:last').append(HTMLonlineURL.replace('%data%',projects.projects[project].url));
+		}
+	}
+};
+
+var bio = {
+	"name":"Ziqi",
+	"role":"Web Developer",
+	"welcomeMessage":"Stay foolish, stay hungry!",
+	"biopic":"http://placekitten.com/g/450/400",
+	"contacts":{
+		"mobile":"123456789",
+		"email":"shaoziqi0628@gmail.com",
+		"github":"novakoki",
+		"twitter":"shaoziqi0628",
+		"location":"Hangzhou, China"
+	},
+	"skills":['C++','HTML/CSS','JavaScript','PHP','Android','Python'],
+	"display":function () {
+		var header = $('#header');
+		var topContacts = $('#topContacts');
+		var footerContacts = $('#footerContacts');
+		header.prepend(HTMLheaderRole.replace('%data%',bio.role));
+		header.prepend(HTMLheaderName.replace('%data%',bio.name));
+		topContacts.append(HTMLmobile.replace('%data%',bio.contacts.mobile));
+		topContacts.append(HTMLemail.replace('%data%',bio.contacts.email));
+		topContacts.append(HTMLgithub.replace('%data%',bio.contacts.github));
+		topContacts.append(HTMLtwitter.replace('%data%',bio.contacts.twitter));
+		topContacts.append(HTMLlocation.replace('%data%',bio.contacts.location));
+		header.append(HTMLbioPic.replace('%data%',bio.biopic));
+		header.append(HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage));
+		header.append(HTMLskillsStart);
+		for(skill in bio.skills)
+			$('#skills-h3').append(HTMLskills.replace('%data%',bio.skills[skill]));
+		footerContacts.append(HTMLmobile.replace('%data%',bio.contacts.mobile));
+		footerContacts.append(HTMLemail.replace('%data%',bio.contacts.email));
+		footerContacts.append(HTMLgithub.replace('%data%',bio.contacts.github));
+		footerContacts.append(HTMLtwitter.replace('%data%',bio.contacts.twitter));
+		footerContacts.append(HTMLlocation.replace('%data%',bio.contacts.location));
+	}
+};
+
+var education = {
+	"schools":[
+	{
+		"name":"Zhejiang University Of Technology",
+		"location":"Hangzhou,China",
+		"degree":"BS",
+		"majors":"Computer Science",
+		"dates":"2013",
+		"url":"www.zjut.edu.cn"
+	}
+	],
+	"onlineCourses":[
+	{
+		"title":"JavaScript Basics",
+		"school":"Udacity",
+		"dates":"2015",
+		"url":"https://www.udacity.com/course/javascript-basics--ud804"
+	},
+	{
+		"title":"Intro to HTML and CSS",
+		"school":"Udacity",
+		"dates":"2015",
+		"url":"https://www.udacity.com/course/intro-to-html-and-css--ud304"
+	},
+	{
+		"title":"From Nand to Tetris / Part I",
+		"school":"The Hebrew University of Jerusalem",
+		"dates":"2015",
+		"url":"https://www.coursera.org/course/nand2tetris1"
+	},
+	{
+		"title":"Computer Organization",
+		"school":"Peking University",
+		"dates":"2015",
+		"url":"https://www.coursera.org/course/pkuco"
+	},
+	{
+		"title":"More Details",
+		"school":"",
+		"dates":"",
+		"url":"http://mooc.guokr.com/user/0181324484/"
+	},
+	],
+	"display":function () {
+		$('#education').append(HTMLschoolStart);
+		var education_entry = $('.education-entry');
+		for(school in education.schools){
+			education_entry.append(HTMLschoolName.replace('%data%',education.schools[school].name)
+				+HTMLschoolDegree.replace('%data%',education.schools[school].degree));
+			education_entry.append(HTMLschoolDates.replace('%data%',education.schools[school].dates));
+			education_entry.append(HTMLschoolLocation.replace('%data%',education.schools[school].location));
+			education_entry.append(HTMLschoolMajor.replace('%data%',education.schools[school].majors));
+		}
+		education_entry.append(HTMLonlineClasses);
+		for(course in education.onlineCourses){
+			education_entry.append(HTMLonlineTitle.replace('%data%',education.onlineCourses[course].title)
+				+HTMLonlineSchool.replace('%data%',education.onlineCourses[course].school));
+			education_entry.append(HTMLonlineDates.replace('%data%',education.onlineCourses[course].dates));
+			education_entry.append(HTMLonlineURL.replace('%data%',education.onlineCourses[course].url));
+		}
+	}
+};
+
+
+bio.display();
+work.display();
+projects.display();
+education.display();
+$('#map-div').append(googleMap);
+
